10c4515e05b388e1536b52e2c558f7afbae08cd1
Mahmoud <mahmodlte@gmail.com>
2024-11-14 09:41:46 +0300

    Solved the problem of summing all numbers to match the TDD cases

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function (fromNum, toNum) {
+  let sum = 0;
+  if (fromNum > toNum) {
+    for (toNum; toNum <= fromNum; toNum++) {
+      sum += toNum;
+    }
+  } else if (
+    Number.isInteger(fromNum) &&
+    Number.isInteger(toNum) &&
+    fromNum > 0
+  ) {
+    for (fromNum; fromNum <= toNum; fromNum++) {
+      sum += fromNum;
+    }
+  } else {
+    return "ERROR";
+  }
 
+  return sum;
 };
+
+console.log(sumAll(1, [2, 400]));
 
 // Do not edit below this line
 module.exports = sumAll;
