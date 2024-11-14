b41be7ae29e90d0e6055f252a0369499d1a8dfb4
Mahmoud <mahmodlte@gmail.com>
2024-11-14 10:03:15 +0300

    Solved the problem of finding the leap year to match the TDD cases

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  let result = false;
+  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+    result = true;
+  } else {
+    result = false;
+  }
+  return result;
 };
-
+console.log(leapYears(1996));
 // Do not edit below this line
 module.exports = leapYears;
