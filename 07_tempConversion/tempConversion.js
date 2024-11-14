0d212633976e010ea31ce637c3fc5b1d0ab47f9f
Mahmoud <mahmodlte@gmail.com>
2024-11-14 10:14:31 +0300

    Solved the problem of temp converstion to match TDD cases

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  const celsiusTemp = ((temp - 32) * 5) / 9;
+  const roundedTemp = Math.round(celsiusTemp * 10) / 10;
+  return roundedTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  const fahrenheitTemp = (temp * 9) / 5 + 32;
+  const roundedTemp = Math.round(fahrenheitTemp * 10) / 10;
+  return roundedTemp;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
